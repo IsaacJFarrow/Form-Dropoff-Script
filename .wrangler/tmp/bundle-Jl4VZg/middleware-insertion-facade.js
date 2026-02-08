24ca861995a63b0b45b4ef96402ad36bc4f60557
Isaac Farrow <isaac@whatifweb.co.nz>
2026-02-09 11:19:11 +1300

    Refactor analytics section in index.html to introduce sub-tabs for Funnel Analysis and Monthly Trends. Removed outdated date filters and added new statistics for better performance tracking. Enhanced layout for improved user experience.

---
+++ .wrangler/tmp/bundle-Jl4VZg/middleware-insertion-facade.js
@@ -0,0 +1,11 @@
+				import worker, * as OTHER_EXPORTS from "C:\\Users\\isaac\\OneDrive\\Desktop\\Projects\\Form Dropoff Script\\.wrangler\\tmp\\pages-WzxSZT\\functionsWorker-0.09969607311401618.mjs";
+				import * as __MIDDLEWARE_0__ from "C:\\Users\\isaac\\AppData\\Roaming\\npm\\node_modules\\wrangler\\templates\\middleware\\middleware-ensure-req-body-drained.ts";
+import * as __MIDDLEWARE_1__ from "C:\\Users\\isaac\\AppData\\Roaming\\npm\\node_modules\\wrangler\\templates\\middleware\\middleware-miniflare3-json-error.ts";
+
+				export * from "C:\\Users\\isaac\\OneDrive\\Desktop\\Projects\\Form Dropoff Script\\.wrangler\\tmp\\pages-WzxSZT\\functionsWorker-0.09969607311401618.mjs";
+				const MIDDLEWARE_TEST_INJECT = "__INJECT_FOR_TESTING_WRANGLER_MIDDLEWARE__";
+				export const __INTERNAL_WRANGLER_MIDDLEWARE__ = [
+					
+					__MIDDLEWARE_0__.default,__MIDDLEWARE_1__.default
+				]
+				export default worker;
---
+++ .wrangler/tmp/pages-cWGdPU/functionsRoutes-0.06796550227350484.mjs
@@ -0,0 +1,11 @@
+import { onRequest as __api___path___js_onRequest } from "C:\\Users\\isaac\\OneDrive\\Desktop\\Projects\\Form Dropoff Script\\functions\\api\\[[path]].js"
+
+export const routes = [
+    {
+      routePath: "/api/:path*",
+      mountPath: "/api",
+      method: "",
+      middlewares: [],
+      modules: [__api___path___js_onRequest],
+    },
+  ]
